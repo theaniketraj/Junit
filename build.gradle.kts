e5a282715dd1c14ec438490d906748f628645aa1
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-07-06 13:36:39 +0530

    Add build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
